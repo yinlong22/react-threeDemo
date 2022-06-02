1f857b65e121e54471b31ef7e839a851ff10734b
shiyinlong <342241857@qq.com>
2022-06-02 15:30:20 +0800

    feat: 删除不必要的代码

--- src/glbBig/3DGlbBig.js
+++ src/glbBig/3DGlbBig.js
@@ -111,12 +111,6 @@ export const TDGlbBig = () => {
     }
 
     return (
-        <>
-            <span className='loader' style={{
-                zIndex: 2, position: 'fixed', width: '100%', height: '100%', top: 0, left: 0, background: '#f1f1f1',
-                display: 'flex', justifyContent: 'center', alignItems: 'center',
-            }}/>
-            <div/>
-        </>
+        <div/>
     )
 }
